86d4e6b08edb2555a72c8334e15bc1f5d238058a
artem-yaremchuk <119439325+artem-yaremchuk@users.noreply.github.com>
2023-09-29 17:29:57 +0300

    update#2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  
+  const startBtn = document.querySelector("button[data-start");
+  const stopBtn = document.querySelector("button[data-stop]");
+  const body = document.body;
+  let timerId = null;
+  
+  startBtn.addEventListener("click", () => {
+    startBtn.setAttribute("disabled", "");
+    timerId = setInterval(() => {
+      const currentColor = getRandomHexColor();
+      body.style.backgroundColor = currentColor;
+    }, 1000);
+  });
+  
+  stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute("disabled");
+  });
+  
